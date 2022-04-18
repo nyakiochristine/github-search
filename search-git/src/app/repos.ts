f43b2b6f92438c626cdaf092ab74c9f6a81170d0
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-18 13:46:34 +0300

    ft:created a model class for repos

---
+++ search-git/src/app/repos.ts
@@ -0,0 +1,13 @@
+export class Repos {
+    
+    constructor(
+        public name:string,
+        public html_url:string,
+        public description: string,
+        public language:string,
+        public forks:number,
+        public created_at: Date
+    ){}
+
+    
+}
