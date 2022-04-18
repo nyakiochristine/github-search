04d7de5839e7b65b7785241b5eb7150507cb11c3
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-18 13:47:46 +0300

    ft:created a model class for user profiles

---
+++ search-git/src/app/user.ts
@@ -0,0 +1,16 @@
+export class User {
+    constructor(
+   public name: string,
+   public followers: string,
+   public public_repos: number, 
+   public url: string,
+   public blog: string,
+   public avatar_url: string,
+   public login: string,
+   public location: string,
+   public gists: number,
+   public created_at: Date,
+){}
+   
+
+}
