fb17b8f73ff5e9852a10cd5478bdbf986e4fd407
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-18 21:22:47 +0300

    patch:fixed type of variable

--- search-git/src/app/user.ts
+++ search-git/src/app/user.ts
@@ -1,7 +1,8 @@
 export class User {
     constructor(
    public name: string,
-   public followers: string,
+   public followers: number,
+   public following: number,
    public public_repos: number, 
    public url: string,
    public blog: string,
